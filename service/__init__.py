5183ed1f5cebcb6c23a662d6106003932832664e
totorocancode <133162270+totorocancode@users.noreply.github.com>
2023-05-15 14:15:54 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -7,6 +7,7 @@ and SQL database
 import sys
 from flask import Flask
 from flask_talisman import Talisman
+from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 
