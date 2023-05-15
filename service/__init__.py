34e96a9f20f8609b8431028d681a784fa6d86c96
totorocancode <133162270+totorocancode@users.noreply.github.com>
2023-05-15 14:16:39 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -17,6 +17,8 @@ app.config.from_object(config)
 
 talisman = Talisman(app)
 
+CORS(app)
+
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
